18988c11b437bc7d456e78bfa05653278d0e6a9f
vaibnak <vaibnakgupta100@gmail.com>
2019-03-18 17:11:06 +0530

    implemented otp code

--- app/src/main/java/com/example/user/testapp/MainActivity.java
+++ app/src/main/java/com/example/user/testapp/MainActivity.java
@@ -13,11 +13,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import id.zelory.compressor.Compressor;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -31,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private String mob;
     private File cfile,myfile;
     private Boolean ispic;
+    private String mVerificationId;
     private StorageReference mStorageRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,13 +61,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void upld(View view) {
-        mob = textInputEditText.getText().toString();
-        if(mob.length() != 10 || (ispic == false)){
-            Toast.makeText(getApplicationContext(), "mobile number not ok", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(getApplicationContext(), "everything alright", Toast.LENGTH_SHORT).show();
-            uploadFile(cfile);
-        }
+//        mob = textInputEditText.getText().toString();
+//        if(mob.length() != 10 || (ispic == false)){
+//            Toast.makeText(getApplicationContext(), "mobile number not ok", Toast.LENGTH_SHORT).show();
+//        }else{
+//            Toast.makeText(getApplicationContext(), "everything alright", Toast.LENGTH_SHORT).show();
+//            uploadFile(cfile);
+//        }
+        sendotp();
     }
 
     public void take_pht(View view) {
@@ -109,4 +116,52 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    private void sendVerificationCode(String mobile) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91" + mobile,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallbacks);
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+            //Getting the code sent by SMS
+            String code = phoneAuthCredential.getSmsCode();
+            Log.i("code: ", code);
+            //sometime the code is not detected automatically
+            //in this case the code will be null
+            //so user has to manually enter the code
+//            if (code != null) {
+//                editTextCode.setText(code);
+//                //verifying the code
+//                verifyVerificationCode(code);
+//            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+
+            Log.i("verification id: ", s);
+            //storing the verification id that is sent to the user
+            mVerificationId = s;
+        }
+    };
+
+
+    public void sendotp(){
+        String mob= textInputEditText.getText().toString();
+        sendVerificationCode(mob);
+    }
+
 }
